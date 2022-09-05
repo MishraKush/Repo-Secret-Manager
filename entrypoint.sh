d9c119b3a281ae40b14fe52a67c2af3a4b0b52f6
Kush Mishra <88425326+MishraKush@users.noreply.github.com>
2022-09-05 17:04:57 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ if [ ${#TEAM} -gt 0 ] && [ ${#REPO} -gt 0 ]
 then
   python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --templateRepo $TEMPLATE_REPO
 else
-  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES
+  python /app/main.py $ACTION --token $TOKEN --names $NAMES --values $VALUES --team $TEAM --templateRepo $TEMPLATE_REPO
 fi
